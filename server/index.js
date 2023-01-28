694377dfb8e4dcb109facc276bd27e27e477fad0
Vladimir <slight1989@gmail.com>
2023-01-29 02:29:12 +0400

    server: user data to front fnc

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
+//data import
+import User from './models/User.js';
+import { dataUser } from './data/index.js'; //mock data
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -33,5 +37,7 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
